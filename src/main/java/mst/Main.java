1335507e8feaa4f9eab353cc40353c52ac3f1b81
Aruana <ZHAKSYLYKARUANA@GMAIL.COM>
2025-10-22 22:50:05 +0500

    Initial commit

--- src/main/java/mst/Main.java
+++ src/main/java/mst/Main.java
@@ -1,17 +1,37 @@
 package mst;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.List;
+
+/**
+ * Main class to run MST algorithms on all test graphs
+ */
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("=== MST Algorithm Comparison ===\n");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Load all test graphs
+        String[] testFiles = {
+                "mst_small_graphs.json",
+                "mst_medium_graphs.json",
+                "mst_large_graphs.json",
+                "mst_extra_large_graphs.json",
+                "mst_test_graphs.json"
+        };
+
+        for (String testFile : testFiles) {
+            try {
+                System.out.println("Loading: " + testFile);
+                List<Graph> graphs = Graph.loadGraphsFromJson(testFile);
+                System.out.printf("Loaded %d graphs from %s%n%n", graphs.size(), testFile);
+
+                MSTComparison comparison = new MSTComparison(graphs);
+                comparison.runComparison();
+
+            } catch (Exception e) {
+                System.err.println("Failed to process " + testFile + ": " + e.getMessage());
+            }
         }
+
+        System.out.println("=== Comparison Complete ===");
     }
 }
