cf2f8274ed29cc42429d02a620fa21f5699935e4
Aruana <ZHAKSYLYKARUANA@GMAIL.COM>
2025-10-22 22:47:28 +0500

    Initial commit

--- src/main/java/mst/Edge.java
+++ src/main/java/mst/Edge.java
@@ -1,4 +1,45 @@
 package mst;
 
-public class Edge {
+/**
+ * Represents an edge in a graph with source, destination, and weight
+ */
+public class Edge implements Comparable<Edge> {
+    private final int source;
+    private final int destination;
+    private final double weight;
+
+    public Edge(int source, int destination, double weight) {
+        this.source = source;
+        this.destination = destination;
+        this.weight = weight;
+    }
+
+    public int getSource() { return source; }
+    public int getDestination() { return destination; }
+    public double getWeight() { return weight; }
+
+    @Override
+    public int compareTo(Edge other) {
+        return Double.compare(this.weight, other.weight);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d-%d: %.2f)", source, destination, weight);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Edge edge = (Edge) obj;
+        return source == edge.source &&
+                destination == edge.destination &&
+                Double.compare(edge.weight, weight) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(source, destination, weight);
+    }
 }
