55516b319a2a0f1d38499097f23d13e6e9d87a4d
Aruana <ZHAKSYLYKARUANA@GMAIL.COM>
2025-10-23 23:02:38 +0500

    Initial commit

--- src/test/java/mst/MSTAlgorithmsTest.java
+++ src/test/java/mst/MSTAlgorithmsTest.java
@@ -4,9 +4,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Unit tests for MST algorithms
- */
+
 class MSTAlgorithmsTest {
 
     @Test
